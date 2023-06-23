3c8d29e22a448b57996707190e92d949994ab1eb
bhushantechnologies <100681398+bhushantechnologies@users.noreply.github.com>
2023-06-23 16:57:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4AYG5GBGWRWYIA3J" # create your own 
-  secret_key = "RGaVK+GpUsu/e0a8k78zAveLIh9MVLzoR8Qs5IuQ" # create your own
+  access_key = "AKIA4AYG5GBGT3YTYS6C" # create your own 
+  secret_key = "DZoSG9yQUqcraytr18ZbCA8ihnkJ8k0WAlP3X1UV" # create your own
 }
